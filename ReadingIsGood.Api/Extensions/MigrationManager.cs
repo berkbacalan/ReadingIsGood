62a74a02ec16141876be02d567b8eca4b0b871fb
Berk Bacalan <berkbacalan@gmail.com>
2023-08-24 20:48:21 +0300

    add migration manager

---
+++ ReadingIsGood.Api/Extensions/MigrationManager.cs
@@ -0,0 +1,23 @@
+﻿using ReadingIsGood.Infrastructure.Data;
+
+namespace ReadingIsGood.Api.Extensions;
+
+public static class MigrationManager
+{
+    public static IHost MigrateDatabase(this IHost host)
+    {
+        using (var scope = host.Services.CreateScope())
+        {
+            try
+            {
+                var orderContext = scope.ServiceProvider.GetRequiredService<OrderContext>();
+                
+            }
+            catch (Exception e)
+            {
+                throw;
+            }
+        }
+        return host;
+    }
+}
--- ReadingIsGood.Api/Program.cs
+++ ReadingIsGood.Api/Program.cs
@@ -1,3 +1,5 @@
+using ReadingIsGood.Api.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +10,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.MigrateDatabase();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
