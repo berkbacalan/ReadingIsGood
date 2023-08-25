c04eee4d1750d587b6db076b749eec53df3b0db9
Berk Bacalan <berkbacalan@gmail.com>
2023-08-25 17:33:50 +0300

    Update repositories

--- ReadingIsGood.Infrastructure/Repositories/BookRepository.cs
+++ ReadingIsGood.Infrastructure/Repositories/BookRepository.cs
@@ -1,4 +1,5 @@
-﻿using ReadingIsGood.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using ReadingIsGood.Domain.Entities;
 using ReadingIsGood.Domain.Repositories;
 using ReadingIsGood.Infrastructure.Data;
 using ReadingIsGood.Infrastructure.Repositories.Base;
@@ -9,5 +10,12 @@ public class BookRepository : Repository<Book>, IBookRepository
 {
     public BookRepository(AppDbContext context) : base(context)
     {
+    }
+    
+    public async Task<IEnumerable<Book>> GetStockByBookId(int bookId)
+    {
+        var bookList = await _dbContext.Books.Where(o => o.Id == bookId)
+            .ToListAsync();
+        return bookList;
     }
 }
--- ReadingIsGood.Infrastructure/Repositories/OrderRepository.cs
+++ ReadingIsGood.Infrastructure/Repositories/OrderRepository.cs
@@ -1,4 +1,5 @@
-﻿using ReadingIsGood.Domain.Entities;
+﻿using Microsoft.EntityFrameworkCore;
+using ReadingIsGood.Domain.Entities;
 using ReadingIsGood.Domain.Repositories;
 using ReadingIsGood.Infrastructure.Data;
 using ReadingIsGood.Infrastructure.Repositories.Base;
@@ -9,5 +10,13 @@ public class OrderRepository : Repository<Order>, IOrderRepository
 {
     public OrderRepository(AppDbContext context) : base(context)
     {
+        
+    }
+
+    public async Task<IEnumerable<Order>> GetOrdersByCustomerId(int customerId)
+    {
+        var orderList = await _dbContext.Orders.Where(o => o.CustomerId == customerId)
+            .ToListAsync();
+        return orderList;
     }
 }
