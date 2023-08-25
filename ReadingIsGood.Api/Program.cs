38e9cb14f9234d07e57b27256590c7dde426c044
Berk Bacalan <berkbacalan@gmail.com>
2023-08-25 17:34:49 +0300

    Add application project dependencies

--- ReadingIsGood.Api/Program.cs
+++ ReadingIsGood.Api/Program.cs
@@ -1,4 +1,5 @@
 using ReadingIsGood.Api.Extensions;
+using ReadingIsGood.Application;
 using ReadingIsGood.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplication();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
