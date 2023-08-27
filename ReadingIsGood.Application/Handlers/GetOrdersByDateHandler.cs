0219268af9ccb38272929f1fc5083cd65b04d846
Berk Bacalan <berkbacalan@gmail.com>
2023-08-27 16:29:47 +0300

    Add get orders by date query and handler

---
+++ ReadingIsGood.Application/Handlers/GetOrdersByDateHandler.cs
@@ -0,0 +1,26 @@
+﻿using AutoMapper;
+using MediatR;
+using ReadingIsGood.Application.Queries;
+using ReadingIsGood.Application.Responses;
+using ReadingIsGood.Domain.Repositories;
+
+namespace ReadingIsGood.Application.Handlers;
+
+public class GetOrdersByDateHandler : IRequestHandler<GetOrdersByDateQuery, IEnumerable<OrderResponse>>
+{
+    private readonly IOrderRepository _orderRepository;
+    private readonly IMapper _mapper;
+
+    public GetOrdersByDateHandler(IOrderRepository orderRepository, IMapper mapper)
+    {
+        _orderRepository = orderRepository;
+        _mapper = mapper;
+    }
+
+    public async Task<IEnumerable<OrderResponse>> Handle(GetOrdersByDateQuery request, CancellationToken cancellationToken)
+    {
+        var orderList = await _orderRepository.GetOrdersByDate(request.StartDate, request.EndDate);
+        var response = orderList.Select(order => _mapper.Map<OrderResponse>(order));
+        return response;
+    }
+}
---
+++ ReadingIsGood.Application/Queries/GetOrdersByDateQuery.cs
@@ -0,0 +1,16 @@
+﻿using MediatR;
+using ReadingIsGood.Application.Responses;
+
+namespace ReadingIsGood.Application.Queries;
+
+public class GetOrdersByDateQuery : IRequest<IEnumerable<OrderResponse>>
+{
+    public DateTime? StartDate { get; set; }
+    public DateTime? EndDate { get; set; }
+
+    public GetOrdersByDateQuery(DateTime? startDate, DateTime? endDate)
+    {
+        StartDate = startDate;
+        EndDate = endDate;
+    }
+}
